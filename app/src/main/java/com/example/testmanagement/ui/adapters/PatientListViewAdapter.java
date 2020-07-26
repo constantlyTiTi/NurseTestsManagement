5e8c689d218c04a6d57a56b707c080040d7fa954
Ting <mycentry1986@gmail.com>
2020-07-26 13:38:50 -0400

    improve patient listView display

--- app/src/main/java/com/example/testmanagement/ui/adapters/PatientListViewAdapter.java
+++ app/src/main/java/com/example/testmanagement/ui/adapters/PatientListViewAdapter.java
@@ -1,7 +1,6 @@
 package com.example.testmanagement.ui.adapters;
 
 import android.app.Activity;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,34 +21,46 @@ public class PatientListViewAdapter extends ArrayAdapter<Patient> {
     private TextView paitentId_tv;
     private TextView patientFirstName_tv;
     private TextView patientLastName_tv;
-    private LinearLayout updateButtonLayout;
-    private LinearLayout deleteButtonLayout;
+    private LinearLayout editViewButtonLayout;
+
     private Activity context;
 
     private List<Patient> patients;
 
     public PatientListViewAdapter(@NonNull Activity context, @NonNull List<Patient> objects) {
         super(context, R.layout.paitent_listview, objects);
+        objects.add(0,null);
         this.patients=objects;
         this.context=context;
     }
 
     public View getView(int position, View view, ViewGroup parent){
-        Button updateButton=new Button(this.context);
-        Button deleteButton=new Button(this.context);
-
         LayoutInflater inflater=this.context.getLayoutInflater();
         View rowView=inflater.inflate(R.layout.paitent_listview, null,true);
         paitentId_tv=(TextView)rowView.findViewById(R.id.patientId_listView_tv);
         patientFirstName_tv=(TextView)rowView.findViewById(R.id.patientFirstName_listView_tv);
-        patientLastName_tv=(TextView)rowView.findViewById(R.id.patientFirstName_listView_tv);
-        updateButtonLayout=(LinearLayout)rowView.findViewById(R.id.updatebtLayout_listView);
-        deleteButtonLayout=(LinearLayout)rowView.findViewById(R.id.deletebtLayout_listView);
-        if(updateButtonLayout!=null){
-            updateButtonLayout.addView(updateButton);
-        }
-        if(deleteButtonLayout!=null){
-            deleteButtonLayout.addView(deleteButton);
+        patientLastName_tv=(TextView)rowView.findViewById(R.id.patientLastName_listView_tv);
+        editViewButtonLayout=(LinearLayout)rowView.findViewById(R.id.editViewbtLayout_listView);
+        if(position!=0){
+            if(editViewButtonLayout!=null){
+                Button editViewButton=new Button(this.context);
+                editViewButton.setText(R.string.patientListViewEditView);
+                editViewButtonLayout.addView(editViewButton);
+            }
+            paitentId_tv.setText(patients.get(position).getPatientId().toString());
+            patientFirstName_tv.setText(patients.get(position).getFirstName());
+            patientLastName_tv.setText(patients.get(position).getLastName());}
+        else {
+/*            TextView editViewTextView=new TextView(this.context);
+            editViewButtonLayout.addView(editViewTextView);
+            editViewTextView.setText(R.string.patientListViewEditView);
+            editViewTextView.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
+            editViewTextView.setHeight(LinearLayout.LayoutParams.MATCH_PARENT);
+            editViewTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            editViewTextView.setTextSize(14);*/
+            patientFirstName_tv.setText(R.string.patientListViewPatientFN);
+            patientLastName_tv.setText(R.string.patientListViewPatientLN);
+            paitentId_tv.setText(R.string.patientListViewPatientId);
         }
         return rowView;
     }
